2b1ce3d35a5bea388f7ec79d18023449bdd9b8d4
jhonaucachiet12-code <jhon.aucachi.et12@gmail.com>
2026-09-07 16:06:50 -0300

    modelo.1

--- GranDT.Core/Model/Jugador.cs
+++ GranDT.Core/Model/Jugador.cs
@@ -3,15 +3,13 @@ namespace GranDT.Core.Model;
 public class Jugador
 {
     public short idJugador {get;set;}
-    public byte idTipo{get;set;}
-    public byte idEquipo{get;set;}
     public required string nombre{get;set;}
     public required string apellido{get;set;}
     public required string apodo{get;set;}
     public DateTime nacimiento{get;set;}
     public decimal cotización{get;set;}
-    public Tipo Tipo {get;set}
-    public Equipo Equipo{get;set;}
+    public required  Tipo tipo {get;set;}
+    public required Equipo equipo{get;set;}
 
 
 }
